432f8103a6beb351b5f751bb873d986529dcdceb
hibby <hibaqku7@gmail.com>
2025-06-15 23:46:40 +0300

    Add root React files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Components/Navbar.jsx'
+import Home from './Components/Home.jsx'
+import Projects from './Components/Projects.jsx'
+import Contact from './Components/Contact.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <Projects/>
+      <Contact/>
+      
+      
+    </div>
+  )
+}
+
+export default App
